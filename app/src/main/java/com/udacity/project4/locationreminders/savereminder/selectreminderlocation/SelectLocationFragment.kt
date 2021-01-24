0501e8849ce61f119d95b20b580bfb40b90785a7
dominic <pagtalunan.d@gmail.com>
2021-01-24 21:14:44 +0800

    updated get location

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -2,18 +2,14 @@ package com.udacity.project4.locationreminders.savereminder.selectreminderlocati
 
 
 import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.content.IntentSender
 import android.content.pm.PackageManager
-import android.content.res.Resources
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -22,13 +18,10 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
-import com.google.android.material.snackbar.Snackbar
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
-import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
-import com.udacity.project4.utils.PermissionManager
 import com.udacity.project4.utils.PermissionManager.Companion.checkDeviceLocationSettingsAndStartGeofence
 import com.udacity.project4.utils.PermissionManager.Companion.foregroundAndBackgroundLocationPermissionApproved
 import com.udacity.project4.utils.PermissionManager.Companion.requestForegroundAndBackgroundLocationPermissions
@@ -38,6 +31,18 @@ import timber.log.Timber
 
 class SelectLocationFragment : BaseFragment(), OnMapReadyCallback,
     OnCompleteListener<LocationSettingsResponse> {
+
+
+    companion object {
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+        private const val DEFAULT_ZOOM = 15
+
+    }
+
+    private lateinit var lastKnownLocation: Location
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+    private var locationPermissionGranted: Boolean = false
+    private lateinit var map: GoogleMap
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
@@ -54,24 +59,29 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback,
 
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
 
 
         val mapFragment = childFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        getLocationPermission()
 //        TODO: put a marker to location that the user selected
 
 
 //        TODO: call this function after the user confirms on the selected location
         onLocationSelected()
-
+        getDeviceLocation()
         return binding.root
     }
 
     private fun checkPermissionsAndZoom() {
         if (foregroundAndBackgroundLocationPermissionApproved(requireContext())) {
-            checkDeviceLocationSettingsAndStartGeofence(activity = requireActivity(),
-            onCompleteListener = this)
+            checkDeviceLocationSettingsAndStartGeofence(
+                activity = requireActivity(),
+                onCompleteListener = this
+            )
         } else {
             requestForegroundAndBackgroundLocationPermissions(requireActivity())
         }
@@ -90,23 +100,28 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback,
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        // TODO: Change the map type based on the user's selection.
         R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
             true
         }
         R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
+
             true
         }
         R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
             true
         }
         R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
             true
         }
         else -> super.onOptionsItemSelected(item)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
         googleMap.addMarker(
             MarkerOptions()
                 .position(LatLng(0.0, 0.0))
@@ -119,13 +134,66 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback,
 
     }
 
-    override fun onComplete(p0: Task<LocationSettingsResponse>) {
-        zoomToPosition()
+    override fun onComplete(task: Task<LocationSettingsResponse>) {
+        task.result?.let {
+            zoomToPosition(it)
+        }
     }
 
-    private fun zoomToPosition() {
-        Timber.d("lets zoom to position")
+    private fun zoomToPosition(result: LocationSettingsResponse) {
     }
 
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(
+                requireActivity().applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
 
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = mFusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result!!
+                        if (lastKnownLocation != null) {
+                            map?.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(
+                                    LatLng(
+                                        lastKnownLocation.latitude,
+                                        lastKnownLocation.longitude
+                                    ), DEFAULT_ZOOM.toFloat()
+                                )
+                            )
+                        }
+                    } else {
+                        Timber.d("Current location is null. Using defaults.")
+                        Timber.e("Exception: %s", task.exception)
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
 }
