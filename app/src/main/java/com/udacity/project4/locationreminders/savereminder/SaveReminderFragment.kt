737e172ce9b26cc1b680994dc5ec1851bb8a1ec4
Dominic <pagtalunan.d@gmail.com>
2021-01-25 13:21:34 +0800

    added geofence on reminder save

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -1,25 +1,48 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingClient
 import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentSaveReminderBinding
+import com.udacity.project4.locationreminders.geofence.GeofenceBroadcastReceiver
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
+
+const val GEOFENCE_RADIUS_IN_METERS = 100f
+val GEOFENCE_EXPIRATION_IN_MILLISECONDS: Long = TimeUnit.HOURS.toMillis(1)
 
 class SaveReminderFragment : BaseFragment() {
+    companion object {
+        internal const val ACTION_GEOFENCE_EVENT =
+            "savereminder.action.ACTION_GEOFENCE_EVENT"
+    }
     //Get the view model this time as a single to be shared with the another fragment
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSaveReminderBinding
-
+    private lateinit var geofencingClient: GeofencingClient
+    private val geofencePendingIntent: PendingIntent by lazy {
+        val intent = Intent(requireContext(), GeofenceBroadcastReceiver::class.java)
+        intent.action = ACTION_GEOFENCE_EVENT
+        // Use FLAG_UPDATE_CURRENT so that you get the same pending intent back when calling
+        // addGeofences() and removeGeofences().
+        PendingIntent.getBroadcast(requireActivity(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,10 +53,12 @@ class SaveReminderFragment : BaseFragment() {
         setDisplayHomeAsUpEnabled(true)
 
         binding.viewModel = _viewModel
+        geofencingClient = LocationServices.getGeofencingClient(requireActivity())
 
         return binding.root
     }
 
+    @SuppressLint("MissingPermission")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = this
@@ -45,36 +70,47 @@ class SaveReminderFragment : BaseFragment() {
 
         binding.saveReminder.setOnClickListener {
             val title = _viewModel.reminderTitle.value
-            val description = _viewModel.reminderDescription
+            val description = _viewModel.reminderDescription.value
             val location = _viewModel.reminderSelectedLocationStr.value
-            val latitude = _viewModel.latitude
-            val longitude = _viewModel.longitude.value
+            val latitude = _viewModel.latitude.value!!
+            val longitude = _viewModel.longitude.value!!
 
 //            TODO: use the user entered reminder details to:
 //             1) add a geofencing request
 //             2) save the reminder to the local db
 
             //pending geofence code.
-//            val geofence = Geofence.Builder()
-//                .setRequestId(currentGeofenceData.id)
-//                .setCircularRegion(currentGeofenceData.latLong.latitude,
-//                    currentGeofenceData.latLong.longitude,
-//                    GeofencingConstants.GEOFENCE_RADIUS_IN_METERS
-//                )
-//                .setExpirationDuration(GeofencingConstants.GEOFENCE_EXPIRATION_IN_MILLISECONDS)
-//                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
-//                .build()
-//
-//            val geofencingRequest = GeofencingRequest.Builder()
-//                .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
-//                .addGeofence(geofence)
-//                .build()
-//
-//            geofencingClient.addGeofences(geofencingRequest, geofencePendingIntent)?.run {
-//                addOnSuccessListener {
-//                    Timber.d("Add Geofence", geofence.requestId)
+            val geofence = Geofence.Builder()
+                .setCircularRegion(
+                    latitude,
+                    longitude,
+                    GEOFENCE_RADIUS_IN_METERS
+                )
+                .setRequestId(location?:"loc")
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+                .setExpirationDuration(GEOFENCE_EXPIRATION_IN_MILLISECONDS)
+                .build()
+
+            val geofencingRequest = GeofencingRequest.Builder()
+                .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+                .addGeofence(geofence)
+                .build()
+
+            geofencingClient.addGeofences(geofencingRequest, geofencePendingIntent)?.run {
+                addOnSuccessListener {
+                    Timber.d("Add Geofence ${geofence.requestId}")
 //                    viewModel.geofenceActivated()
-//                }
+                    _viewModel.validateAndSaveReminder(ReminderDataItem(title,description,location,latitude,longitude))
+                    findNavController().navigate(R.id.action_saveReminderFragment_to_reminderListFragment)
+                }
+
+                addOnFailureListener{
+                    Timber.e("Error adding geofence")
+
+                }
+            }
+
+
         }
     }
 
